8390266719e347ba9e9986be7f6a56482e8ac413
Leonardo Ribeiro <leonardo.andradegcr@gmail.com>
2026-05-18 21:54:04 -0300

    Add interface repository funcionario

---
+++ gerenciador-Veterinarios/src/main/java/org/iftm/gerenciadorveterinarios/repositories/FuncionarioRepository.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package org.iftm.gerenciadorveterinarios.repositories;
+
+
+import org.iftm.gerenciadorveterinarios.entities.Funcionario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Integer>{
+
+}
